e7ab84ba81fa82d9e4b11d92f9e52a7d263b2745
Shehan Gamage <shehangamage55@gmail.com>
2017-06-25 20:40:36 +0530

    Use count method

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -18,6 +18,12 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
     console.log('Unable to fetch todos', err);
   });
 
+  db.collection('Todos').find().count().then((count)=>{
+    console.log(`Todos count: ${count}`);  
+  },(err)=>{
+    console.log('Unable to fetch todos', err);
+  });
+
 
 //  db.close();
 });
