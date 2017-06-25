cada68b34c6e9df3bb1c5575b35466611317aba0
Shehan Gamage <shehangamage55@gmail.com>
2017-06-25 20:18:49 +0530

    Fetching Data Basic

---
+++ playground/mongodb-find.js
@@ -0,0 +1,21 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+  if(err){
+    console.log('Unable to connect MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').find().toArray().then((docs)=>{
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  },(err)=>{
+    console.log('Unable to fetch todos', err);
+  });
+
+
+//  db.close();
+});
