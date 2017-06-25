16e637ccb5388035b380a50ce9829f84f06958a0
Shehan Gamage <shehangamage55@gmail.com>
2017-06-25 19:59:19 +0530

    Basic MongoDB functions with node

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+  if(err){
+    console.log('Unable to connect MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  },(err, result)=>{
+    if(err){
+      return console.log('Unable to insert todo', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.collection('Users').insertOne({
+    name: 'Shehan Gamage',
+    age: '24',
+    location: "Warakapola"
+  },(err, result)=>{
+    if(err){
+      return console.log('Unable to insert user.');
+    }
+
+    console.log(result.ops);
+    console.log(result.ops[0]._id.getTimestamp());
+  });
+
+  db.close();
+});
