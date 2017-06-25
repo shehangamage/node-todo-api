aba34b0d34ce08ea552ddcfd30dbac4c455e94ce
Shehan Gamage <shehangamage55@gmail.com>
2017-06-25 20:28:55 +0530

    Find with arguments

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -9,7 +9,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
 
   console.log('Connected to MongoDB server');
 
-  db.collection('Todos').find().toArray().then((docs)=>{
+  db.collection('Todos').find({completed: false}).toArray().then((docs)=>{
     console.log('Todos');
     console.log(JSON.stringify(docs, undefined, 2));
   },(err)=>{
