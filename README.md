1e3a23fab99eac25939893229bbc6c69b495246d
Shehan Gamage <shehangamage55@gmail.com>
2017-06-26 13:00:52 +0530

    deleteOne() & findOneAndDelete()

--- README.md
+++ README.md
@@ -63,5 +63,17 @@ db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result)=>{
 #### deleteOne()
 
 ```
+ db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result)=>{
+   console.log(result);
+ });
+
+```
+
+#### findOneAndDelete()
+
+```
+db.collection('Users').findOneAndDelete({_id: new ObjectID('594fc3ad8597611f50587598')}).then((result)=>{
+  console.log(JSON.stringify(result, undefined, 2));
+});
 
 ```
--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -10,13 +10,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
   console.log('Connected to MongoDB server');
 
   //deleteMany
-  db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result)=>{
-    console.log(result);
-  });
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result)=>{
+  //   console.log(result);
+  // });
   //deleteOne
-
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result)=>{
+  //   console.log(result);
+  // });
   //findOneAndDelete
-
+  // db.collection('Users').deleteMany({name: 'Shehan Gamage'}).then((result)=>{
+  //   console.log(result);
+  // });
+  db.collection('Users').findOneAndDelete({_id: new ObjectID('594fc3ad8597611f50587598')}).then((result)=>{
+    console.log(JSON.stringify(result, undefined, 2));
+  });
 
 //  db.close();
 });
