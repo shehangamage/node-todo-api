c5ce96354ef53b9f75956558dc7c1bbfd85a46b6
Shehan Gamage <shehangamage55@gmail.com>
2017-06-28 19:57:19 +0530

    Overide toJSON() method

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -30,6 +31,15 @@ var UserSchema = new mongoose.Schema({
     }
   }]
 });
+
+//Overide toJSON() method -------------
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
 
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
