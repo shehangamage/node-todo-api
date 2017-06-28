0e45de0fe168a7bbfda18778020eb1ee3540cefa
Shehan Gamage <shehangamage55@gmail.com>
2017-06-28 23:17:54 +0530

    Making Todo Routes Private

--- server/server.js
+++ server/server.js
@@ -20,9 +20,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((docs) => {
@@ -32,20 +33,25 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req, res)=>{
-  Todo.find().then((todos)=>{
+app.get('/todos',authenticate, (req, res)=>{
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos)=>{
     res.send({todos});
   },(err)=>{
    res.status(400).send(err);
   });
 });
 
-app.get('/todos/:id', (req, res)=>{
+app.get('/todos/:id',authenticate, (req, res)=>{
   var id = req.params.id;
   if(!ObjectId.isValid(id)){
     return res.status(404).send('ID not valid.');
   }
-  Todo.findById(id).then((todo)=>{
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo)=>{
     if(todo === null){
         return res.status(404).send('ID not found.');
     }
@@ -56,12 +62,15 @@ app.get('/todos/:id', (req, res)=>{
 
 });
 
-app.delete('/todos/:id',(req, res)=>{
+app.delete('/todos/:id',authenticate, (req, res)=>{
   var id =  req.params.id;
   if(!ObjectId.isValid(id)){
     return res.status(404).send('ID not valid');
   }
-  Todo.findByIdAndRemove(id).then((todo)=>{
+  Todo.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo)=>{
     if(todo === null){
       return res.status(404).send('ID not found');
     }
